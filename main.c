bc7b2b8739b2aac7921037391bc16771cf8c6fc7
AleLanzini00 <87639991+AleLanzini00@users.noreply.github.com>
2021-07-26 12:09:58 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,16 +1,17 @@
 /*NOTE
- * Per aggiornare la classifica sostituisco il MASSIMO
- * Scrivere algoritmo trovamassimo: data la calssifica e k(dim), reistuisce la posiz del valore
-    con la distanza maggiore
+ * CLASSIFCA NON FUNZIONA: -verificare che venga gestita correttamente
+ *                         -algortimo funziona con test prova (grafi semplici 3x3)
+ *passare ad algoritmo la matrice con PUNTATORE
  *Testare con valori prova che la classifca funzioni correttamente
- * Scrivere algortimo che calcola distanza
 */
 #include <stdio.h>
 #include<stdlib.h>
 #include<math.h>
 
-void TopK(){
-}
+struct Grafo{
+    int ID;
+    int distanza;
+};
 
 int numcifre(int n){
     int cifre=1;
@@ -25,28 +26,69 @@ int numcifre(int n){
     }
 }
 
-int calcoladist(int dim){
-    int distanza=0;
-        //algoritmo da fare, anche i paramentri
-    return distanza;
+void visita(int nodo, int size, int matr[size][size],int* vettdistp){       //IMPLEM USO DI PUNT A MATRICE
+    int i;
+    for(i=0;i<size;i++){    //guardo la riga del nodo interessato
+        if(matr[nodo][i]!=0 && i!=nodo){
+            if((*(vettdistp+i) > (matr[nodo][i]+*(vettdistp+nodo)) || *(vettdistp+i)==-1)){     //allora esploro i suoi adiacenti
+                *(vettdistp+i)=matr[nodo][i]+*(vettdistp+nodo); //aggiorno distanza nodo esplorato
+                visita(i,size,matr,vettdistp);  //esploro adiacenti nuovo nodo
+
+            }
+        }
+    }
 }
 
- struct Grafo{
-    int ID;
-    int distanza;
-};
+int algoritmo(int dim,int mat[dim][dim]){
+    int disttot=0;
+    int n=0;
+    //inizializzo i nodi
+    int vettdist[dim];   //ogni poszione contiene la distanza di un nodo
+    for(n=0;n<dim;n++){     //pongo tutte le distanze a -1 (valore simbolico)
+        if(n==0){
+            vettdist[n]=0;
+        }
+        else
+            vettdist[n]=-1;
+    }
+    //esplorazione
+    int r=0;
+    int c;
+    for(c=1;c<dim;c++){
+        if(mat[r][c]!=0){
+            if(vettdist[c]==-1 || vettdist[c]>mat[r][c]){
+                vettdist[c]=mat[r][c]; //segno distanza nodi adiacenti a zero
+                visita(c,dim,mat, vettdist);
+            }
+        }
+    }/*
+    for(n=0;n<dim;n++){
+        //prova di stampa
+        printf("\nDistanza nodo %d: %d",n,vettdist[n]);
+        disttot=disttot+vettdist[n];
+    }*/
+    return disttot;
+}
 
 int trovamassimo(struct Grafo vet[],int dim){   //restituisce la posizione del massimo
-    int max=0;
-        //da scrivere
-    return max;
+    int maxp=0;
+    int maxd=-1;
+    int i;
+    for(i=0;i<dim;i++){
+        if(vet[i].distanza>maxd){
+            maxp=i;
+            maxd=vet[i].distanza;
+        }
+    }
+    return maxp;
 }
 
 int main() {
     int d,k,c,i,posiz,dist;
     int maxdist=0;
     int id=0;   //usato per contare i grafi inseriti, e quindi ne restituisce l'id
-    printf("Inserire d e k:\n");
+
+    //printf("Inserire d e k:\n");
     scanf("%d %d",&d,&k);   //leggo d e k
     int maxdim=(d*10)+(d-1); //2^32=4.294.967.295, quindi 10 cifre per arco, più d-1 virgole
     char s[maxdim];
@@ -54,7 +96,7 @@ int main() {
     int matrice[d][d];  //matrice per salvare input
 
     //leggo un comando
-    printf("Inserire un comando (Aggiungigrafo o TopK):\n");
+    //printf("Inserire un comando (Aggiungigrafo o TopK):\n");
     scanf("%s",s);
     while(s[0]=='A'){   //AggiungiGrafo
         //leggo la matrice  per righe
@@ -76,9 +118,8 @@ int main() {
             }
             printf("\n");
         }*/
-
         //CALCOLO DIST E GESTISCO TOPK
-        dist=calcoladist(d); //devo passare la matrice
+        dist=algoritmo(d,matrice); //devo passare la matrice
         if(id<k){   //inizializzo la classifica con i primi K grafi
             classifica[id].ID=id;
             classifica[id].distanza=dist;
@@ -94,13 +135,14 @@ int main() {
                 maxdist=trovamassimo(classifica,k); //ricalcolo il peggiore
             }
         }
-
-
         //leggo nuovo comando
         id++;
         scanf("%s",s);
     }
     if(s[0]=='T'){
         //STAMPA LA CLASSIFICA
+        for(i=0;i<k;i++){
+            printf("%d ",classifica[i].ID);
+        }
     }
 }
