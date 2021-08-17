6e9dead397ad2f595412e2095c69ab6ddec7da27
AleLanzini00 <87639991+AleLanzini00@users.noreply.github.com>
2021-08-17 10:12:30 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
  * ERRORE: eseguendo djikstra il programma si blocca cercando di leggere la posiz d+1 della matrice
  *
  *      -fino a riga 116 sembra funzionare tutto (heapsize corretto e accessibile)
- *      -PROBLEMA: devo usare heapsize-1 per c+accedere a A[heapszie]!!!
+ *      -PROBLEMA: devo usare heapsize-1 per accedere a A[heapszie]!!!
  *
  *  FORSE MEGLIO INIT Q IN DJIKSTRA E NON IN MAIN
  * rivedere infinito
@@ -13,12 +13,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define infinito 4000000
+int heapsize;
 
 struct nodo{
     int nome;
     int dist;
     int prev;
-    int* heapsizep;
+    //int* heapsizep;
 };
 
 int trovapos(struct nodo *A, int dim,int nomenodo){     //ricerca per nome del nodo e ne restituisce la posizione
@@ -42,13 +43,13 @@ void min_heapify(struct nodo *A, int posiz){
     int min;
     int l = (posiz*2)+1;
     int r = (posiz*2)+2; //figli sx e dx
-    if(l<*(A->heapsizep) && (((A+l)->dist) < (A+posiz)->dist)){
+    if(l <= heapsize && (((A+l)->dist) < (A+posiz)->dist)){
         min = l;
     }
     else{
         min = posiz;
     }
-    if(r<*(A->heapsizep) && (A+r)->dist<(A+min)->dist) {
+    if(r <= heapsize && (A+r)->dist<(A+min)->dist) {
         min = r;
     }
     if(min!=posiz){
@@ -83,21 +84,21 @@ void heap_decrease_key(struct nodo* A,int posiz,struct nodo* node){
 }
 
 void min_heap_insert(struct nodo *A,struct nodo* node){   //aggiunta di un nodo al minheap
-    *(A->heapsizep) = *(A->heapsizep) + 1;
-    ((A + *(A->heapsizep))->dist) = infinito;   //A[A.heapsize] = infinito
-    ((A + *(A->heapsizep))->nome) = -1;
-    ((A + *(A->heapsizep))->prev) = -1;
-    heap_decrease_key(A, *(A->heapsizep), node);
+    heapsize = heapsize + 1;
+    ((A + heapsize)->dist) = infinito;   //A[A.heapsize] = infinito
+    ((A + heapsize)->nome) = -1;
+    ((A + heapsize)->prev) = -1;
+    heap_decrease_key(A, heapsize, node);
 }
 
 struct nodo heap_extract_min(struct nodo *A){
     struct nodo minimo;
-    if(*(A->heapsizep) < 0){
+    if(heapsize < 0){
         printf("\nErrore underflow");
     }
     minimo = *A; //min=A[0];
-    *A = *(A + *(A->heapsizep)-1);     //A[0]=[A.heapsize]
-    *(A->heapsizep) = (*(A->heapsizep))-1;
+    *A = *(A + heapsize);     //A[0]=[A.heapsize]
+    heapsize = heapsize - 1;
     min_heapify(A,0);
     return minimo;
 }
@@ -107,7 +108,8 @@ void dijkstra(int dim, int mat[dim][dim], struct nodo *A, struct nodo *G){
     int i;
     int riga;
     struct nodo u;
-    *(A->heapsizep)=0;   //inizializzo Q=0
+    heapsize=-1;   //inizializzo Q=0
+                    // -1 perchè il vet inizia da zero, così le posizioni coincidono
     //gestisco nodo 0
     G->dist=0;
     G->prev=-1;     //S.PREV = NIL
@@ -118,8 +120,8 @@ void dijkstra(int dim, int mat[dim][dim], struct nodo *A, struct nodo *G){
         (G+i)->prev=-1;             //predecessore NIL
         min_heap_insert(A,(G+i));   //inserisco tutti i nodi nello heap
     }
-    //test ricerca errore
-    while(*(A->heapsizep)>0){   //DUBBIO SU QUESTA CONDIZOONE
+
+    while(heapsize>-1){   //DUBBIO SU QUESTA CONDIZOONE
         u = heap_extract_min(A);  //estraggo nodo con dist minima
         riga = u.nome;  //per l'n-esimo nodo trovo gli adiacenti nell'n-esima riga
         int pos;
@@ -138,7 +140,6 @@ void dijkstra(int dim, int mat[dim][dim], struct nodo *A, struct nodo *G){
 }
 
 int main() {
-    int heapsize;
     int d;
     printf("\nInserire d: ");
     scanf("%d", &d);
@@ -159,7 +160,6 @@ int main() {
                 scanf("%d", &matrice[i][c]);
             }
         }
-
         //stampo matrice per verifica
         /*printf("\nMatrice inserita: \n");
         for (i = 0; i < d; i++) {
@@ -168,14 +168,15 @@ int main() {
             }
             printf("\n");
         }*/
+
         //init min_heap e grafo:
         heapsize=0;
         for (i = 0; i < d; i++) {
-            Q[i].heapsizep = &heapsize;   //in ogni nodo tengo un puntatore alla varaibile heapsize
+            //Q[i].heapsizep = &heapsize;   //in ogni nodo tengo un puntatore alla varaibile heapsize
             grafo[i].nome = i;                //do il nome a ogni nodo
             grafo[i].dist = -1;
             grafo[i].prev = -1;
-            grafo[i].heapsizep = &heapsize;
+            //grafo[i].heapsizep = &heapsize;
         }
         //uso l'algoritmo di Dijkstra
         dijkstra(d, matrice, Q, grafo);
@@ -185,16 +186,8 @@ int main() {
             if (grafo[i].dist == infinito) {
                 grafo[i].dist = 0;    //gestisce nodi non collegati
             }
-            printf("\nDistanza nodo %d: %d", i, grafo[i].dist);
+            //printf("\nDistanza nodo %d: %d", i, grafo[i].dist);
             tot = tot + grafo[i].dist;
-        }
-        printf("\nDISTANZA TOT: %d\n", tot);
-        //test matrice
-        for (i = 0; i < d; i++) {
-            for (c = 0; c < d; c++) {
-                printf("%d ", matrice[i][c]);
-            }
-            printf("\n");
         }
     }
 }
